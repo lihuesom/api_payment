6aec2445b2a49e47c9f690fd6106a31f7f35c872
lihuesom <linahueso03@gmail.com>
2023-12-21 18:32:21 -0500

    Validando la marca

--- transactions/views.py
+++ transactions/views.py
@@ -5,14 +5,20 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from django.db import IntegrityError
 from .serializers import PaymentSerializer
+from creditcard import CreditCard
 
 @api_view(['POST'])
+#@authentication_classes([]) 
+#@permission_classes([AllowAny])
 def paymentProcess(request):
     try: 
+        card_number = request.data.get('card_number')
+        card = CreditCard(card_number)
+        #if card.is_valid:
         serializer = PaymentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'Pago registrado correctamente'}, status=status.HTTP_202_ACCEPTED)
+            return Response({'message': 'Pago registrado correctamente','Tarjeta': card.brand}, status=status.HTTP_202_ACCEPTED)
         else:
             return Response({'message': 'Error en los datos del pago', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
     except IntegrityError as e:
